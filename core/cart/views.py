82659cf03a60be866cda4f5a079cae3eca699ae3
Student28 <Student28@college.local>
2024-09-10 09:38:51 +0300

    adding in/deleting from cart added

--- core/cart/views.py
+++ core/cart/views.py
@@ -1,10 +1,36 @@
 from typing import Any
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
 from .models import Cart
-from django.contrib.auth.models import User
+from products.models import Product
 
 class CartList(generic.TemplateView):
     def get(self, request):
+        try:
+            Cart.objects.get(user=request.user)
+        except:
+            Cart.objects.create(user=request.user)
         context = {'cart_items':Cart.objects.get(user=request.user)}
         return render(request, 'cartlist.html', context)
+    
+def AddToCart(request, product_id):
+    if request.method == 'POST':
+        product = Product.objects.get(pk=product_id)
+        try:
+            user_cart = Cart.objects.get(user=request.user)
+            user_cart.product.add(product)
+        except:
+            cart = Cart.objects.create(user=request.user)
+            cart.product.add(product)
+    return redirect('products_list')
+
+def DeleteFromCart(request, product_id):
+    if request.method == 'POST':
+        products_from_cart = Cart.objects.all()[0]
+        product = Product.objects.get(pk=product_id)
+
+        products_from_cart.product.remove(product)
+
+        return redirect('cart')
+    
+ 
--- core/products/urls.py
+++ core/products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
+from cart.views import *
 urlpatterns = [
     path('', ProductList.as_view(), name='products_list'),
-    path(r'<int:pk>/', Product_info.as_view(), name='product_info')
+    path(r'<int:pk>/', Product_info.as_view(), name='product_info'),
+    path('add/<int:product_id>/', AddToCart, name='add'),
+    path('delete/<int:product_id>/', DeleteFromCart, name='delete'),
 ]
